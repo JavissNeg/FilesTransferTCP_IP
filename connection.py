18e9d9a0960eedd48da3f5bd4f01c57807a48da8
Javiss Negrete <negretbtino@hotmail.com>
2024-02-22 21:06:04 -0600

    First commit

---
+++ connection.py
@@ -0,0 +1,57 @@
+import socket
+
+send = True
+receive = True
+
+host = 'localhost'
+port = 5000
+
+def server_connection():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    
+    while receive:
+        try:
+            s.bind((host, port))
+            s.listen(5)
+            client, address = s.accept()
+            
+            print('Connected to', address)
+            
+            client.close()
+            break
+        except:
+            print('Connection not found. Try again')
+
+def client_connection():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    
+    while send:
+        try:
+            s.connect((host, port))
+            s.sendall(b'Hello, world')
+            
+            print('Conected to server')
+            
+            s.close()
+            break
+        except:
+            print('Connection not found. Try again')
+
+
+
+option = 0
+while option != 3:
+    print('1. Send file to server')
+    print('2. Receive file from server')
+    print('3. Exit')
+    
+    option = int(input('Enter an option: '))
+
+    if option == 1:
+        server_connection()
+    elif option == 2:
+        client_connection()
+    elif option == 3:
+        print('Exiting...')
+    else:
+        print('Invalid option')
